ed5549e83c07f49611f5528d15a2245f37dd98ba
CanalQb <qrodrigob@gmail.com>
2025-10-16 20:39:33 -0300

    Add initial PHP HTML template in a.php

---
+++ teste/a.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Meu Site PHP</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Bem-vindo ao meu site em PHP!</h1>
+  <p><?php echo "Hoje é " . date("d/m/Y"); ?></p>
+</body>
+</html>
