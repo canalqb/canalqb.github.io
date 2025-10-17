8096f79261c5bac6cacd1398ef21a3deee472a87
CanalQb <qrodrigob@gmail.com>
2025-10-16 21:50:40 -0300

    Change page title and update CSS styles
    Updated the title and improved CSS styles for better readability.

--- teste/php/phps/head.php
+++ teste/php/phps/head.php
@@ -2,11 +2,11 @@
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8" />
-  <title>Meu Site PHP WASM</title>
+  <title>Minha Página PHP-WASM</title>
   <style>
-    body { font-family: sans-serif; margin: 0; padding: 0; background: #f4f4f4; }
-    header, footer { background: #222; color: white; padding: 1rem; }
-    nav { background: #444; color: white; padding: 1rem; }
-    main { padding: 2rem; }
+    body { font-family: Arial, sans-serif; background: #f0f0f0; color: #333; margin: 0; padding: 0;}
+    nav { background: #004466; color: white; padding: 1rem; }
+    main { padding: 1rem; }
+    footer { background: #222; color: white; text-align: center; padding: 1rem; position: fixed; width: 100%; bottom: 0;}
   </style>
 </head>
